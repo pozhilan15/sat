0311ac32471bc41120a9f09412c631e691b04a77
pozhilan15 <pozhilanjj95@gmail.com>
2020-05-26 21:58:23 +0530

    project design

---
+++ src/org/lang/LaanguageInfo.java
@@ -0,0 +1,16 @@
+package org.lang;
+
+
+public class LaanguageInfo {
+	public void tamilLanguage() {
+	System.out.println("tamil language 90");
+	}
+public void englishLangauage() {
+	System.out.println("english Langauage 95");
+
+}
+public void hindiLanguage() {
+	// TODO Auto-generated method stub
+System.out.println("hindi language 45");
+}
+}
---
+++ src/org/lang/StateDetails.java
@@ -0,0 +1,22 @@
+package org.lang;
+
+public class StateDetails {
+public void southIndia() {
+	// TODO Auto-generated method stub
+System.out.println("southIndia is 1500");
+}
+public void northIndia() {
+	// TODO Auto-generated method stub
+System.out.println("northindia is 1400");
+}
+public static void main(String[] args) {
+	StateDetails s=new StateDetails();
+	s.southIndia();
+	s.northIndia();
+	
+LaanguageInfo l=new LaanguageInfo();
+l.tamilLanguage();
+l.englishLangauage();
+l.hindiLanguage();
+}
+}

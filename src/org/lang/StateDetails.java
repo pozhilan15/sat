3d39a69efd52ec44a719b7b6f4efe436b8b9dae2
pozhilan15 <pozhilanjj95@gmail.com>
2020-05-28 20:52:08 +0530

    vasanth ac

--- src/org/lang/StateDetails.java
+++ src/org/lang/StateDetails.java
@@ -3,11 +3,11 @@ package org.lang;
 public class StateDetails {
 public void southIndia() {
 	// TODO Auto-generated method stub
-System.out.println("southIndia is 1500");
+System.out.println("southIndia is 1530");
 }
 public void northIndia() {
 	// TODO Auto-generated method stub
-System.out.println("northindia is 1400");
+System.out.println("northindia is 1450");
 }
 public static void main(String[] args) {
 	StateDetails s=new StateDetails();
